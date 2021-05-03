40273996619974e7d1b62c58456e30a86bd6b488
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-05-03 12:35:18 -0500

    Markdown functionality working

--- models/event.js
+++ models/event.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+
 const marked = require('marked');
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+
+const dompurify = createDomPurify(new JSDOM().window)
 
 const EventSchema = new mongoose.Schema({
   title: {
@@ -22,6 +27,10 @@ const EventSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
   }
 })
 
@@ -29,6 +38,10 @@ EventSchema.pre('validate', function (next) {
   if(this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true })
   }
+  if(this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+  }
   next()
 })
+
 module.exports = mongoose.model('Event', EventSchema)
