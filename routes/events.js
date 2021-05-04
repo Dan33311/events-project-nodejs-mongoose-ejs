daafdbb30a8085c245d59a9bedf2a50dc8c12165
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2021-05-03 19:46:50 -0500

    c8 PUT and new POST/PUT function

--- routes/events.js
+++ routes/events.js
@@ -3,10 +3,16 @@ const Event = require('./../models/event')
 const router = express.Router()
 
 
-
 router.get('/new', (req, res) => {
   res.render('events/new', { event: new Event })
 })
+
+
+router.get('/edit/:id', async (req, res) => {
+  const event = await Event.findById(req.params.id)
+  res.render('events/edit', { event: event })
+})
+
 
 router.get('/:slug', async (req, res) => {
   const event = await Event.findOne({ slug: req.params.slug })
@@ -14,24 +20,38 @@ router.get('/:slug', async (req, res) => {
   res.render('events/show', { event: event })
 })
 
-router.post('/', async (req, res) => {
-  let event = new Event({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown
-  })
-  try {
-    event = await event.save()
-    res.redirect(`events/${event.slug}`)
-  } catch (error) {
-    console.log(error)
-    res.render('events/new', { event: event})
-  }
-})
+
+router.post('/', async (req, res, next) => {
+  req.event = new Event()
+  next()
+}, saveAndRedirect('new'))
+
+
+router.put('/:id', async (req, res, next) => {
+  req.event = await Event.findById(req.params.id)
+  next()
+}, saveAndRedirect('edit'))
+
 
 router.delete('/:id', async (req, res) => {
   const event = await Event.findByIdAndDelete(req.params.id)
   res.redirect('/')
 })
+
+
+function saveAndRedirect(path) {
+  return async (req, res) => {
+    let event = req.event
+    event.title = req.body.title
+    event.description = req.body.description
+    event.markdown = req.body.markdown
+    try {
+      event = await event.save()
+      res.redirect(`/events/${event.slug}`)
+    } catch (error) {
+      res.render(`events/${path}`, { event: event})
+    }
+  }
+}
 
 module.exports = router
